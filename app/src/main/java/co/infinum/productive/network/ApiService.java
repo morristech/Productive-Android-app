4b85a14173b81900d30640b572dd6d13860dc389
skliba <kliba.stefano@gmail.com>
2015-11-04 13:08:38 +0100

    styles fix

--- app/src/main/java/co/infinum/productive/network/ApiService.java
+++ app/src/main/java/co/infinum/productive/network/ApiService.java
@@ -11,9 +11,6 @@ import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
 import retrofit.http.POST;
 
-/**
- * Created by dino on 12/10/15.
- */
 public interface ApiService {
 
     @FormUrlEncoded
