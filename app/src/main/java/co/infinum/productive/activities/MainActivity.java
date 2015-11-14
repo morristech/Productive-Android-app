7583cb31623b72750a394716763641ee37fdfc40
skliba <kliba.stefano@gmail.com>
2015-11-14 01:24:03 +0100

    PR fixes <ana>

--- app/src/main/java/co/infinum/productive/activities/MainActivity.java
+++ app/src/main/java/co/infinum/productive/activities/MainActivity.java
@@ -2,16 +2,23 @@ package co.infinum.productive.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import co.infinum.productive.R;
 import co.infinum.productive.adapters.ViewPagerAdapter;
+import co.infinum.productive.fragments.MoreFragment;
+import co.infinum.productive.fragments.NotificationsFragment;
+import co.infinum.productive.fragments.ProjectsFragment;
+import co.infinum.productive.fragments.TasksFragment;
 
 public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
 
@@ -21,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     @Bind(R.id.viewPager)
     ViewPager pager;
 
+    private ArrayList<Fragment> fragmentArrayList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +37,9 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         ButterKnife.bind(this);
 
         initTabs();
+        initFragmentList();
 
-        final PagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        final PagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), fragmentArrayList);
         pager.setAdapter(adapter);
         pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.setOnTabSelectedListener(this);
@@ -38,11 +48,20 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         pager.setCurrentItem(1);
     }
 
+    private void initFragmentList() {
+        fragmentArrayList = new ArrayList<>();
+
+        fragmentArrayList.add(new TasksFragment());
+        fragmentArrayList.add(new ProjectsFragment());
+        fragmentArrayList.add(new NotificationsFragment());
+        fragmentArrayList.add(new MoreFragment());
+    }
+
     private void initTabs() {
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_tasks_inactive).setText(R.string.my_tasks_tab));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_projects_active).setText(R.string.projects_tab));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_notifications_inactive).setText(R.string.notifications_tab));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_more_inactive).setText(R.string.more_tab));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.tasks_tab_selector).setText(R.string.my_tasks_tab));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.projects_tab_selector).setText(R.string.projects_tab));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.notifications_tab_selector).setText(R.string.notifications_tab));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.more_tab_selector).setText(R.string.more_tab));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 
@@ -60,42 +79,17 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     @Override
     public void onTabSelected(TabLayout.Tab tab) {
         pager.setCurrentItem(tab.getPosition());
-        switch (tab.getPosition()) {
-            case 0:
-                tab.setIcon(R.drawable.ic_tasks_active);
-                break;
-            case 1:
-                tab.setIcon(R.drawable.ic_projects_active);
-                break;
-            case 2:
-                tab.setIcon(R.drawable.ic_notifications_active);
-                break;
-            default:
-                tab.setIcon(R.drawable.ic_more_active);
-                break;
-        }
     }
 
     @Override
     public void onTabUnselected(TabLayout.Tab tab) {
-        switch (tab.getPosition()) {
-            case 0:
-                tab.setIcon(R.drawable.ic_tasks_inactive);
-                break;
-            case 1:
-                tab.setIcon(R.drawable.ic_projects_inactive);
-                break;
-            case 2:
-                tab.setIcon(R.drawable.ic_notifications_inactive);
-                break;
-            default:
-                tab.setIcon(R.drawable.ic_more_inactive);
-                break;
-        }
+
     }
 
     @Override
     public void onTabReselected(TabLayout.Tab tab) {
 
     }
+
+
 }
