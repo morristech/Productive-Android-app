af74b149d3f8d6c343497c43c3b9644d0007fb2e
mjurinic <jurinic.mislav@gmail.com>
2015-12-05 22:36:15 +0100

    unused imports...

--- app/src/main/java/co/infinum/productive/activities/MainActivity.java
+++ app/src/main/java/co/infinum/productive/activities/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
