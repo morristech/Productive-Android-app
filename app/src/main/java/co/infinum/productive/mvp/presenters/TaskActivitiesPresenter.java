03c0a1a569ec91439f75ddcf8bd2cd82c2ffd71b
Mislav Jurinic <jurinic.mislav@gmail.com>
2015-12-29 03:35:33 +0100

    fetching activities (only demo visuals)

---
+++ app/src/main/java/co/infinum/productive/mvp/presenters/TaskActivitiesPresenter.java
@@ -0,0 +1,9 @@
+package co.infinum.productive.mvp.presenters;
+
+/**
+ * Created by mjurinic on 28.12.15..
+ */
+public interface TaskActivitiesPresenter {
+
+    void getTaskActivities(int projectId, int taskId);
+}
